8cfed9a581e17319b8911a078a672533c6441f62
Hunter Fox <hefoxed@gmail.com>
2017-02-21 14:45:51 -0800

    Add age confirmation

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
 import React, { PropTypes } from 'react'
-import { injectGlobal } from 'styled-components'
+import { Button } from 'react-toolbox/lib/button';
 
-injectGlobal`
-  body {
-    margin: 0;
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      is18: localStorage.getItem('is18') == 'true',
+    };
+    console.log(this.state);
   }
-`
 
-const App = ({ children }) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
+  confirmAge = () => {
+    localStorage.setItem('is18', 'true')
+    this.setState({is18: true});
+  };
+
+  render() {
+    var that = this;
+    if (!that.state.is18) {
+      return (
+        <div>
+          This site contains 18+ material blahlbah.
+          <Button href='http://google.com/' icon="exit_to_app">Leave</Button>
+          <Button onClick={this.confirmAge}>Confirm Age</Button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        {that.props.children}
+      </div>
+    );
+  }
 }
+
 
 App.propTypes = {
   children: PropTypes.oneOfType([
